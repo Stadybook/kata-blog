c3d5e754086dff1deecc269cc5759de17c9293b8
Anastasia Serebrennikova <Serebrennikovaav01@mail.ru>
2023-02-24 14:56:36 +0300

    set tags adding

--- src/components/CreateArticle/CreateArticle.js
+++ src/components/CreateArticle/CreateArticle.js
@@ -1,14 +1,15 @@
+/* eslint-disable no-undef */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { Button } from 'antd';
+import { Tag } from 'antd';
 
 import getId from '../../helpFunctions/getId';
 
 import style from './CreateArticle.module.scss';
 
-const tags = [1, 2];
 function CreateArticle() {
     const {
         register,
@@ -25,17 +26,48 @@ function CreateArticle() {
         reset();
     };
 
-    const tagList = tags.map((tag) => {
+    const [tagList, setTagList] = useState([]);
+    const [tagValue, setTagValue] = useState({ name: '', id: '' });
+
+    const onDelete = (id) => {
+        const index = tagList.findIndex((el) => el.id === id);
+        const newData = [
+            ...tagList.slice(0, index),
+            ...tagList.slice(index + 1),
+        ];
+        setTagList(newData);
+    };
+
+    const onAddTag = () => {
+        setTagList((list) => [...list, tagValue]);
+        setTagValue({ name: '', id: '' });
+    };
+
+    const onChange = (name) => {
         const id = getId();
+        const newTag = {
+            name,
+            id,
+        };
+        setTagValue(newTag);
+    };
+
+    const tags = tagList.map((tag) => {
+        const { id } = tag;
         return (
-            <li key={id} className={style.tag}>
-                <label htmlFor={id}>
-                    <input placeholder='tag' id={id} />
-                </label>
-                <Button danger>Delete</Button>
+            <li key={tag.id} className={style.tag}>
+                <Tag>{tag.name}</Tag>
+                <button
+                    className={style.delete}
+                    type='button'
+                    onClick={() => onDelete(id)}
+                >
+                    Delete
+                </button>
             </li>
         );
     });
+
     return (
         <section className={style.container}>
             <h5 className={style.title}>Create new article</h5>
@@ -51,6 +83,7 @@ function CreateArticle() {
                         placeholder='Title'
                     />
                 </label>
+                <p>{errors?.title?.message}</p>
                 <label htmlFor='description'>
                     <span> Short description</span>
                     <input
@@ -62,23 +95,47 @@ function CreateArticle() {
                         placeholder='Short description'
                     />
                 </label>
-                <label htmlFor='Text'>
+                <p>{errors?.description?.message}</p>
+                <label htmlFor='text'>
                     <span> Text</span>
-
                     <textarea
                         type='text'
                         className={errors?.title ? style.danger : null}
-                        {...register('Text', {
+                        {...register('text', {
                             required: 'This is required.',
                         })}
                         placeholder='Text'
                     />
                 </label>
+                <p>{errors?.text?.message}</p>
                 <div className={style.tags}>
                     <span>Tags</span>
-                    <div>
-                        <ul>{tagList}</ul>
-                        <button type='button'>Add tags</button>
+                    <div className={style.tags__list}>
+                        <ul>{tags}</ul>
+                        <div>
+                            <label htmlFor='tag'>
+                                <input
+                                    {...register('tag', {
+                                        required: false,
+                                        pattern: {
+                                            value: /^[a-zA-Z0-9]+$/,
+                                            message:
+                                                'You can use only english letters and digits without spaces and other symbols',
+                                        },
+                                    })}
+                                    value={tagValue.name}
+                                    placeholder='tag'
+                                    onChange={(e) => onChange(e.target.value)}
+                                />
+                            </label>
+                            <button
+                                className={style.add}
+                                type='button'
+                                onClick={onAddTag}
+                            >
+                                Add tags
+                            </button>
+                        </div>
                     </div>
                 </div>
 
